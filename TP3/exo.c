48a8ed1f052b8c2db376f17b807f32a9980635fd
AntoninOswald <antoninoswald@student.uclouvain.be>
2022-02-16 19:14:34 +0100

    Ajout de solutions

--- TP3/exo.c
+++ TP3/exo.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 
 
 void print_digit(int number){
@@ -62,6 +64,9 @@ char *buf_strcpy(const char *src) {
         return NULL;
     }
 
+
+
+
     
     
 
@@ -87,11 +92,192 @@ char *buf_strcpy(const char *src) {
 }
 
 
-int main()
+
+
+int pal(char *str){
+
+
+    int length;
+    int i, j;
+        
+
+
+    if (str == NULL)
+    {
+        return -1;
+    }
+    /* Pour copier une chaine de caractère, on peut utiliser strcopy*/
+    
+    /*   D'abord il faut retirer les espaces*/
+
+    char *str2;
+
+    length = strlen(str);
+
+    str2 = (char *)malloc(length*sizeof(char)+1); /* Ne pas oublier le +1 + Comment faire quand on ne peut pas utiliser malloc ?*/ 
+
+    if (str2 == NULL)
+    {
+        return -1;
+    }
+
+    j = 0;
+
+    for (i = 0; i < length +1; i++)
+    {
+        if(str[i] != ' ')  /*     NE PAS OUBLIER DE METTRE DES CHAR ' ' et PAS DES " "*/ 
+        {
+            str2[j] = str[i];
+            j++;
+            
+        }
+    }
+
+    str2[j] = '\0';/*   Utile ?*/
+
+    printf("%s\n", str);
+
+    printf("%s\n", str2);
+
+    
+
+    int k;
+    int l = strlen(str2);
+
+    for (k = 0 ; k < l+1 ; k++)
+    {
+
+        if (str2[k] != str2[l-k-1])
+        {
+
+            free(str2);
+            return 0;
+            
+        }
+    }
+    free(str2);
+    return 1;
+}
+
+int strcasecmp(const char *s1, const char *s2){
+    int i;
+    for (i = 0; s1[i] && s2[i]; i++){
+        if (s1[1] == s2[i] || (s1[i] ^32) == s2[i]){
+            continue;
+        } else{
+            break;
+        }
+    }
+    if (s1[i] == s2[i]){
+        return 0;
+    }
+
+    if ((s1[i] | 32) < (s2[i] | 32)){
+        return -1;
+    }
+    return 1;
+}
+
+
+int pal2(char *str){ /*   Fonction à mettre sur Inginious, car utiliser la version avec pointeur/malloc ne compile pas*/
+
+    if (str == NULL){
+        return -1;
+    }
+
+    int len = strlen(str);
+    if (len == 0){
+        return -1;
+    }
+
+    int left = 0;
+    int right = len -1;
+
+    while(left < right){
+        char str_left = str[left];
+        char str_right = str[right];
+
+        if (str_left == ' '){
+            left++;
+        }else if (str_right == ' '){
+            right--;
+        } else if (str_left != str_right){
+            return 0;
+        } else{
+            left++;
+            right--;
+        }
+    }
+
+    return 1;
+
+
+}
+
+
+/* TP3*/
+
+
+void *calloc2(size_t nmemb, size_t size){
+
+    if ((nmemb == 0) || (size == 0)){
+        return NULL;
+    }
+
+    void *a = malloc(nmemb*size);
+    if (a == NULL){
+        return NULL;
+    }
+
+    return a;
+    free(a);
+
+
+
+}
+
+void *sleepy_malloc(size_t s){
+    int c = 0;
+    while (c < 10){
+        void* a = malloc(s)
+        if (a == NULL){
+            sleep(5);
+        } else{
+            return a;
+            free(a);
+        }
+        c++;
+    }
+
+    return NULL;
+    free(a);
+}
+
+
+
+
+
+
+
+
+
+int main(int argc, char **argv)
 {
     printf("Hello world!\n");
 
     print_digit(10);
+
+    char *test = " ka y a k";
+    char *t = " djqksldjsqkl";
+    char *a = NULL;
+
+    int d = pal(test);
+
+    printf("%d\n", d);
+
+    printf("%d\n", pal(t));
+
+    printf("%d\n", pal(a));
 
     
 
@@ -99,3 +285,5 @@ int main()
 
 
 }
+
+
---
+++ TP3/test.c
@@ -0,0 +1,59 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+void swap(int *i, int *j){
+
+    int local = *j;
+    int local2 = *i;
+
+    *i = local;
+    *j = local2;
+
+
+
+
+}
+
+struct fract_t {
+    int num;
+    int denum;
+};
+
+void swap2(struct fract_t *a, struct fract_t *b){
+
+
+    struct fract_t temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+
+
+
+int main(int argc, char **argv)
+{
+    printf("Hello world!\n");
+
+    /*int i;
+
+    for (i = 0; i < argc; i++){
+        i = i+1;
+        if (i == argc -1 || i == argc -2 ){
+            printf("%s\n", argv[i]);
+        } else{
+            printf("%s ", argv[i]);
+        }
+        
+    } */
+
+    
+
+    return 0;
+
+
+}
