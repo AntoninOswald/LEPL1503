481391fe98f80e2e778da71d0a340f404c30daf2
AntoninOswald <antonin.oswald@student.uclouvain.be>
2022-02-22 12:44:35 +0100

    S3

--- TP3/exo.c
+++ TP3/exo.c
@@ -5,6 +5,8 @@
 
 #include <unistd.h> /* Permet d'utiliser sleep*/
 
+#include <inttypes.h>
+
 
 
 void print_digit(int number){
@@ -245,6 +247,264 @@ void *sleepy_malloc(size_t s){
 }
 
 
+/**
+* Structure node
+*
+* @next: pointer to the next node in the list, NULL if last node_t
+* @value: value stored in the node
+*/
+/*typedef struct node {
+    struct node *next;
+    int value;
+} node_t;
+
+/**
+* Structure list
+*
+* @first: first node of the list, NULL if list is empty
+* @size: number of nodes in the list
+*/
+/*
+typedef struct list {
+    struct node *first;
+    int size;
+} list_t;
+
+/*
+* Create a new node containing the value @value.
+*
+* @value: value stored in the node
+* @next : pointer to next node, initialised to NULL.
+* @return: returns the pointer to the newly created node, or NULL if an error occured.
+*/
+
+/*
+node_t *init_node(int value){
+
+    node_t *first;
+
+    first = (struct node *)malloc(sizeof(struct node));
+
+    if (first == NULL ){
+        return NULL;
+    }
+
+    first->next = NULL;
+    first->value = value;
+
+    return first;
+    free(first);
+
+
+}
+
+/*
+* Add a node at the head of the list @list and increment the number of nodes in the list (member `size`)
+*
+* @l: linked list
+* @value: value to add to the list
+*
+* @return: 0 if success, 1 otherwise
+*/
+/*
+int add_node(list_t *list, int value) {
+
+    if (list == NULL){
+        return 1;
+
+    } else if (list == NULL){
+        list->first = init_node(value);
+        list->size = 1;
+        return 0;
+    } else{
+        node_t *n = malloc(sizeof(node_t));
+        if (n == NULL){
+            return 1;
+        } else{
+            n->value = value;
+            n->next = list->first;
+            list->first = n;
+            (list->size)++;
+            return 0;
+        }
+
+        free(n);
+    }
+
+
+}
+ */
+
+
+
+
+
+/*
+ * exo sur les arbres binaires
+ */
+
+/*
+* Node has a value, @value, and two children, @left and @right.
+* All the children of @left and itself have a smaller value than the node and all the children of @right and itself have a larger value than node
+*/
+typedef struct node{
+    int value;
+    struct node* left; // to smaller values
+    struct node* right; // to larger values
+} node_t;
+
+
+
+typedef struct bt{
+    struct node* root;
+} bt_t;
+
+
+/*
+* Return 1 if there is a node containing @value in @tree and 0 otherwise.
+*/
+/*int contains(bt_t* tree, int value){
+
+    struct node* init = malloc(sizeof(struct node));
+
+    if(init == NULL){
+        return 0;
+    }
+    init = tree->root;
+
+
+    struct bt* rightTree;
+    struct bt* leftTree;
+
+    rightTree = malloc(sizeof(struct bt));
+    if (rightTree == NULL){
+        return 0;
+    }
+
+
+    leftTree = malloc(sizeof(struct bt));
+    if (leftTree == NULL){
+        return 0;
+    }
+
+    if(tree->root->value == value){
+        return 1;
+    } else{
+
+        if(init->right != NULL && init->left != NULL){
+            rightTree->root = init->right;
+            leftTree->root = init->left;
+
+            if (contains(rightTree,value) == 0){
+                return contains(leftTree,value);
+            } else{
+                return 1;
+            }
+
+        } else if(init->right == NULL && init->left == NULL){
+
+            if(init->value == value){
+                return 1;
+            } else{
+                return 0;
+            }
+        } else if (init->right == NULL && init->left != NULL){
+            return contains(leftTree,value);
+        } else{
+            return contains(rightTree,value);
+        }
+
+
+    }
+
+    free(leftTree);
+    free(rightTree);
+    free(init);
+
+
+}  */
+
+int search(int value, struct node *root){
+
+    if (root != 0){
+        if (value == root->value){
+            return 1;
+        } else if (value < root->value){
+            return search(value, root->left);
+        } else{
+            return search(value,root->right);
+        }
+    } else{
+        return 0;
+    }
+}
+
+int contains(bt_t* tree, int value){
+    if (tree == NULL){
+        return 0;
+    } else{
+        return search(value,tree->root);
+    }
+
+}
+
+
+
+
+
+/* LAST S3 */
+
+
+typedef struct product {
+    char *name;
+    double price;
+} product_t;
+
+
+double getPtrPrice(product_t *ptrProd){
+
+    return ptrProd->price;
+
+}
+
+double getPrice(product_t prod){
+
+    return prod.price;
+
+
+}
+
+char* getPtrName(product_t *ptrProd){
+
+    char *named = malloc(sizeof(char)*(strlen(ptrProd->name)+1));
+
+    if (named == NULL){
+        return NULL;
+    }
+
+    strcpy(named,ptrProd->name);
+
+    return named;
+}
+
+
+char* getName(product_t prod){
+
+    char *named = malloc(sizeof(char)*(strlen(prod.name)+1));
+
+    if (named == NULL){
+        return NULL;
+    }
+
+    strcpy(named,prod.name);
+
+    return named;
+    
+}
+
+
+
+
 
 
 
@@ -269,6 +529,9 @@ int main(int argc, char **argv)
     printf("%d\n", pal(t));
 
     printf("%d\n", pal(a));
+
+    node_t *p = init_node(3);
+    printf("%d\n",p->value);
 
     
 
--- TP3/test.c
+++ TP3/test.c
@@ -5,65 +5,72 @@
 
 
 
-
-void swap(int *i, int *j){
-
-    int local = *j;
-    int local2 = *i;
-
-    *i = local;
-    *j = local2;
-
-
-
-
-}
-
-struct fract_t {
-    int num;
-    int denum;
-};
-
-void swap2(struct fract_t *a, struct fract_t *b){
-
-
-    struct fract_t temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-
-
-
-int main(int argc, char **argv)
-{
-    printf("Hello world!\n");
-
-    int i;
-
-    for (i = 0; i < argc; i++){
-        i = i+1;
-        if (i == argc -1 || i == argc -2 ){
-            printf("%s\n", argv[i]);
-        } else{
-            printf("%s ", argv[i]);
-        }
-        
+int equals(int a, int b){
+    if (a==b){
+        return 0;
+    } else{
+        return 1;
     }
-    printf("Test");
+}
+
+int greaterThan(int a, int b){
+    if (a >= b){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+int greater(int a, int b){
+    if (a > b){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+int lessThan(int a, int b){
+    if (a <= b){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+int less(int a, int b){
+    if (a < b){
+        return 0;
+    } else{
+        return 1;
+    }
+}
+
+int notEqual(int a, int b){
+    if (a != b ){
+        return 0;
+    } else{
+        return 1;
+    }
+}
 
 
 
+int main(int argc, char **argv){
+
+    
+    int v1 = atoi(argv[1]);
+    int v2 = atoi(argv[3]);
+    char *command;
+    command = argv[2];
 
     
 
+    if (strcmp(command, "-eq") == 0){return equals(v1,v2);}
+    if (strcmp(command,"-ge") == 0){return greaterThan(v1,v2);}
+    if (strcmp(command,"-gt") == 0){return greater(v1,v2);}
+    if (strcmp(command,"-le")==0){return lessThan(v1,v2);}
+    if (strcmp(command,"-lt") == 0){return less(v1,v2);}
+    if (strcmp(command,"-ne") == 0){return notEqual(v1,v2);}
     
-
-    
-
-    
-
-    return 0;
-
 
 }
